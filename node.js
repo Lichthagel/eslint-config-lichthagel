3ecd13ca645b3d1fde81e463c19228f833a2c1ef
Lichthagel <lichthagel@tuta.io>
2023-06-08 14:36:05 +0200

    improved node ruleset

--- node.js
+++ node.js
@@ -1,7 +1,22 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["./index.js"],
+  extends: ["./index.js", "plugin:n/recommended-module"],
   env: {
     node: true,
+  },
+  rules: {
+    "n/no-new-require": "warn",
+    "n/no-path-concat": "warn",
+    "n/no-process-env": "warn",
+    "n/no-sync": "warn",
+    "n/prefer-global/buffer": "warn",
+    "n/prefer-global/console": "warn",
+    "n/prefer-global/process": "warn",
+    "n/prefer-global/text-decoder": "warn",
+    "n/prefer-global/text-encoder": "warn",
+    "n/prefer-global/url": "warn",
+    "n/prefer-global/url-search-params": "warn",
+    "n/prefer-promises/dns": "warn",
+    "n/prefer-promises/fs": "warn",
   },
 };
