350eed96a7213ca9543b204f5434baee25c8d6ea
Lichthagel <lichthagel@tuta.io>
2023-06-26 17:58:44 +0200

    base without import, disabled several unicorn rules

--- index.js
+++ index.js
@@ -2,78 +2,9 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  env: {
-    es2022: true,
-  },
-  plugins: ["import", "unicorn"],
-  extends: [
-    "eslint:recommended",
-    "plugin:import/recommended",
-    "plugin:promise/recommended",
-    "plugin:unicorn/recommended",
-    "prettier",
-  ],
-  settings: {
-    "import/resolver": {
-      node: {},
-    },
-    "import/ignore": [
-      "node_modules",
-      "\\.(json|css|jpg|png|gif|eot|svg|ttf|woff|woff2)$",
-    ],
-  },
-  parserOptions: {
-    ecmaVersion: 13,
-    sourceType: "module",
-  },
+  plugins: ["import"],
+  extends: ["./base.js", "plugin:import/recommended"],
   rules: {
-    "array-callback-return": "warn",
-    "no-await-in-loop": "warn",
-    "no-constant-binary-expression": "warn",
-    "no-duplicate-imports": "error",
-    "no-promise-executor-return": "warn",
-    "no-self-compare": "warn",
-    "no-unmodified-loop-condition": "warn",
-    "arrow-body-style": "warn",
-    "default-case-last": "warn",
-    "dot-notation": "warn",
-    "eqeqeq": "warn",
-    "logical-assignment-operators": "warn",
-    "no-bitwise": "warn",
-    "no-var": "warn",
-    "prefer-arrow-callback": "error",
-    "prefer-const": "error",
-    "prefer-destructuring": [
-      "warn",
-      { AssignmentExpression: { array: false, object: false } },
-    ],
-    "prefer-exponentiation-operator": "warn",
-    "prefer-object-spread": "error",
-    "prefer-promise-reject-errors": "warn",
-    "prefer-regex-literals": "warn",
-    "prefer-rest-params": "error",
-    "prefer-spread": "error",
-    "prefer-template": "error",
-    "quote-props": ["warn", "consistent-as-needed"],
-    "require-await": "warn",
-    "spaced-comment": "warn",
-    "yoda": "warn",
-
-    "no-tabs": ["error", { allowIndentationTabs: true }],
-    "quotes": ["warn", "double"],
-
-    "promise/prefer-await-to-then": "warn",
-
-    "unicorn/filename-case": [
-      "error",
-      {
-        cases: {
-          kebabCase: true,
-          pascalCase: true,
-          camelCase: true,
-        },
-      },
-    ],
-    "unicorn/prevent-abbreviations": "off",
+    "import/no-named-as-default": "off",
   },
 };
