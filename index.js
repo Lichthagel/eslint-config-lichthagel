4889d0a7cb19ef73d439e6ebd334856d0db95d92
Lichthagel <lichthagel@tuta.io>
2023-07-01 10:45:11 +0200

    inline config

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
+  extends: ["./base.js"],
   plugins: ["import"],
-  extends: ["./base.js", "plugin:import/recommended"],
   rules: {
-    "import/no-named-as-default": "off",
+    // analysis/correctness
+    "import/no-unresolved": "error",
+    "import/named": "error",
+    "import/namespace": "error",
+    "import/default": "error",
+    "import/export": "error",
+
+    // red flags (thus, warnings)
+    "import/no-named-as-default": "warn",
+    "import/no-named-as-default-member": "off",
+    "import/no-duplicates": "warn",
   },
 };
