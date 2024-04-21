87f7c8f471c24eee565c846181c9111d8e53715b
Lichthagel <hey@lichthagel.de>
2024-04-21 20:20:47 +0200

    lint ts files

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,30 @@
 // @ts-check
-
-import index from "./dist/index.js";
+import path from "node:path";
+import url from "node:url";
+import ts from "typescript-eslint";
 import node from "./dist/node.js";
+import typescript from "./dist/typescript.js";
 
-/** @type import("eslint").Linter.FlatConfig[] */
-export default [...index, ...node, { ignores: ["dist"] }];
+export default [
+  ...typescript,
+  ...node,
+  {
+    rules: {
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+    },
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: path.dirname(url.fileURLToPath(import.meta.url)),
+      },
+    },
+  },
+  {
+    files: ["eslint.config.js"],
+    ...ts.configs.disableTypeChecked,
+  },
+  {
+    ignores: ["dist/**/*"],
+  },
+];
