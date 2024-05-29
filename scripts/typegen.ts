2c3fcaf30a0cefe9c90974b60fb23b11172b7a27
Lichthagel <hey@lichthagel.de>
2024-05-29 22:06:23 +0200

    adjust typegen script

--- scripts/typegen.ts
+++ scripts/typegen.ts
@@ -1,20 +1,49 @@
-import { ESLint } from "eslint";
+import { ESLint, Linter } from "eslint";
 import { builtinRules } from "eslint/use-at-your-own-risk";
 import { pluginsToRulesDTS } from "eslint-typegen/core";
 import fs from "node:fs/promises";
+
+const reduceConfigToPlugins = (config: Linter.FlatConfig | Linter.FlatConfig[]): Record<string, ESLint.Plugin> => {
+  if (!Array.isArray(config)) {
+    return "plugins" in config && config.plugins !== undefined ? config.plugins : {};
+  }
+
+  let plugins = {};
+
+  for (const entry of config) {
+    if ("plugins" in entry && entry.plugins !== undefined) {
+      plugins = {
+        ...plugins,
+        ...entry.plugins,
+      };
+    }
+  }
+
+  return plugins;
+};
 
 const plugins: Record<string, ESLint.Plugin> = {
   "": {
     rules: Object.fromEntries(builtinRules.entries()),
   },
-  "perfectionist": await import("eslint-plugin-perfectionist").then((mod) => mod.default as ESLint.Plugin),
   "react": await import("eslint-plugin-react").then((mod) => mod.default as ESLint.Plugin),
   "react-hooks": await import("eslint-plugin-react-hooks").then((mod) => mod.default as ESLint.Plugin),
-  ...(await import("eslint-plugin-n").then((mod) => mod.default.configs["flat/recommended-module"].plugins)),
-  ...(await import("@stylistic/eslint-plugin").then((mod) => mod.default.configs["all-flat"].plugins)),
-  ...(await import("typescript-eslint").then((mod) => mod.configs.recommended[0].plugins)),
-  ...(await import("eslint-plugin-unicorn").then((mod) => mod.configs["flat/recommended"].plugins as Record<string, ESLint.Plugin>)),
+  ...reduceConfigToPlugins(await import("eslint-plugin-perfectionist/configs/recommended-natural").then((mod) => mod.default as Linter.FlatConfig[])),
+  ...reduceConfigToPlugins(await import("eslint-plugin-n").then((mod) => mod.default.configs["flat/recommended-module"] as Linter.FlatConfig[])),
+  ...reduceConfigToPlugins(await import("@stylistic/eslint-plugin").then((mod) => mod.default.configs["all-flat"] as Linter.FlatConfig[])),
+  ...reduceConfigToPlugins(await import("typescript-eslint").then((mod) => mod.configs.recommended as Linter.FlatConfig[])),
+  ...reduceConfigToPlugins(await import("eslint-plugin-unicorn").then((mod) => mod.configs["flat/recommended"] as Linter.FlatConfig[])),
 };
+
+// eslint-disable-next-line no-console
+console.log(
+  `Generating rule definitions with plugins: ${
+    Object
+      .keys(plugins)
+      .map((s) => `'${s}'`)
+      .join(", ")
+  }`,
+);
 
 const dts = await pluginsToRulesDTS(plugins, {
   includeAugmentation: true,
